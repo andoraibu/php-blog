20ad120d0ef913e2d42681b9ccd0d268dcd4bed6
Andrew <elbroandrew@gmail.com>
2024-09-27 14:15:47 +1000

    загрузилась стартовая страница, но не остальные

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'dsn' => 'mysql:host=127.0.0.1;port=3306;dbname=blog_php',
+    'dsn' => 'mysql:host=mysql;port=3306;dbname=php_blog',
     'username' => 'root',
     'password' => '1234',
 ];
