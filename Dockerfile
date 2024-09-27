0aa61464ec152361aa6d096f06ca307110d7a72c
Andrew <elbroandrew@gmail.com>
2024-09-27 13:05:56 +1000

    init sql db

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM mysql:latest
 
-ENV MYSQL_DATABASE php_blog
 
 
-COPY ./sql /docker-entrypoint-initdb.d/
+ADD ./sql/queries.sql /docker-entrypoint-initdb.d
--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'dsn' => 'mysql:host=127.0.0.1;dbname=blog_php',
+    'dsn' => 'mysql:host=127.0.0.1;port=3306;dbname=blog_php',
     'username' => 'root',
     'password' => '1234',
 ];
