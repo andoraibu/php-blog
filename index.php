206558111a897d157a56738e9a47fad033aedbda
Andrew <elbroandrew@gmail.com>
2024-09-27 21:52:09 +1000

    refactor: переделал под twig-view

--- index.php
+++ index.php
@@ -1,92 +1,73 @@
 <?php
 
+
 use Blog\LatestPosts;
 use Blog\PostMapper;
-use Blog\Slim\TwigMiddleware;
-use DI\ContainerBuilder;
+// use Blog\Slim\TwigMiddleware;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use DI\Container;
+use Slim\Views\Twig;
 use Twig\Environment;
-use Twig\Loader\FilesystemLoader;
+use DI\ContainerBuilder;
+use Slim\Views\TwigMiddleware;
 
-require __DIR__ . '/vendor/autoload.php';
 
-$builder = new ContainerBuilder();
-$builder->addDefinitions('config/di.php');
 
-$container = $builder->build();
-
-AppFactory::setContainer($container);
-
-$view = $container->get(Environment::class);
-
-$config = include 'config/database.php';
-$dsn = $config['dsn'];
-$username = $config['username'];
-$password = $config['password'];
+require __DIR__ . "/vendor/autoload.php";
 
 try {
-    $connection = new PDO($dsn, $username, $password);
+    // $connection = new PDO($dsn, $username, $password);
+    $connection = new PDO('mysql:host=127.0.0.1;dbname=php_blog', "root", "1234");
     $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    if ($connection){
+        echo "Connected to DB\n";
+    }
 }catch (PDOException $exception){
     echo 'Database error: ' . $exception->getMessage();
     die();
 }
 
-$app = AppFactory::create();
-$app->add(new TwigMiddleware($view));
+require __DIR__ . '/vendor/autoload.php';
 
-$app->get('/', function (Request $request, Response $response) use ($view, $connection) {
+// Create Container
+$container = new Container();
+
+// Set view in Container
+$container->set(Twig::class, function() {
+    return Twig::create(__DIR__ . '/templates', ['cache' => false]);
+});
+
+// Create App from container
+$app = AppFactory::createFromContainer($container);
+
+// Add Twig-View Middleware
+$app->add(TwigMiddleware::create($app, $container->get(Twig::class)));
+// $app->add(new TwigMiddleware($view));
+
+// Add other middleware
+$app->addRoutingMiddleware();
+$app->addErrorMiddleware(true, true, true);
+
+// $view = $container->get(Environment::class);
+
+$app->get('/', function ($request, $response) use ($connection){
     $latestPosts = new LatestPosts($connection);
+
     $posts = $latestPosts->get(2);
-    $body = $view->render('index.twig', [
-        'posts' => $posts
-    ]);
-    $response->getBody()->write($body);
-    return $response;
+    $twig = $this->get(Twig::class);
+    // $body = $view->render('index.twig', [
+    //     'posts' => $posts
+    // ]);
+    // $response->getBody()->write($body);
+
+
+    return $twig->render($response, 'index.twig', $posts);
+
 });
 
-$app->get('/about', function (Request $request, Response $response) use ($view) {
-    $body = $view->render('about.twig', [
-        'name' => "Andrew"
-    ]);
-    $response->getBody()->write($body);
-    return $response;
-});
 
-$app->get('/blog[/{page}]', function (Request $request, Response $response, $args) use ($view, $connection) {
-    $postMapper = new PostMapper($connection);
-    $page = isset($args['page']) ? (int) $args['page'] : 1;
-    $limit = 2;
-    $posts = $postMapper->getList($page, $limit, 'DESC');
-    $totalCount = $postMapper->getTotalCount();
-    $body = $view->render('blog.twig', [
-        'posts' => $posts,
-        'pagination' => [
-            'current' => $page,
-            'paging' => ceil($totalCount / $limit)
-        ]
-    ]);
-    $response->getBody()->write($body);
-    return $response;
-});
-
-$app->get('/{url_key}', function (Request $request, Response $response, $args) use ($view, $connection) {
-    $postMapper = new PostMapper($connection);
-    $post = $postMapper->getByUrlKey((string) $args['url_key']);
-
-    if(empty($post)){
-        $body = $view->render('not-found.twig');
-    }else{
-        $body = $view->render('post.twig', [
-            'post' => $post
-        ]);
-    }
-
-    $response->getBody()->write($body);
-    return $response;
-});
 
 $app->run();
