1ffb959fdd852cf51c70ea1c6555d7d71d289b7d
Andrew <elbroandrew@gmail.com>
2024-10-01 12:36:11 +1000

    add path to image

--- sql/seed.sql
+++ sql/seed.sql
@@ -1,2 +1,2 @@
 INSERT INTO post
-VALUES (1, "my title 1", "url1", "img path", "some text content", "description", STR_TO_DATE('2020-05-28 12:00', '%Y-%m-%d %H:%i'));
+VALUES (1, "my title 1", "url1", "public/images/christmas-1869902_960_720.jpg", "some text content", "description", STR_TO_DATE('2020-05-28 12:00', '%Y-%m-%d %H:%i'));
