0f01b4d2bb25bcf942e441e5e51d7b4b8739af28
Andrew <elbroandrew@gmail.com>
2024-10-09 21:46:59 +1000

    добавил права на папку с изображениями

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ use Blog\Slim\TwigMiddleware;
 use DI\ContainerBuilder;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\UploadedFileInterface;
 use Slim\Factory\AppFactory;
 use Twig\Environment;
 
@@ -16,6 +17,9 @@ $builder = new ContainerBuilder();
 $builder->addDefinitions('config/di.php');
 
 $container = $builder->build();
+
+$container->set('upload_directory', __DIR__ . '/public/images');
+
 AppFactory::setContainer($container);
 $view = $container->get(Environment::class);
 
@@ -53,6 +57,23 @@ $app->get('/', function (Request $request, Response $response) use ($view, $conn
 $app->get('/post', function (Request $request, Response $response) use ($view) {
     $body = $view->render('new_post.twig', []);
     $response->getBody()->write($body);
+    return $response;
+});
+
+$app->post('/post', function (Request $request, Response $response) use ($view) {
+
+    $directory = $this->get('upload_directory');
+    $uploadedFiles = $request->getUploadedFiles();
+
+    // handle single input with single file upload
+    $uploadedFile = $uploadedFiles['image'];
+    if ($uploadedFile->getError() === UPLOAD_ERR_OK) {
+        $filename = moveUploadedFile($directory, $uploadedFile);
+        $response->getBody()->write('Uploaded: ' . $filename . '<br/>');
+    }
+    
+    // $body = $view->render('new_post.twig', []);
+    // $response->getBody()->write($body);
     return $response;
 });
 
@@ -98,6 +119,24 @@ $app->get('/{url_key}', function (Request $request, Response $response, $args) u
     return $response;
 });
 
+/**
+ * Moves the uploaded file to the upload directory and assigns it a unique name
+ * to avoid overwriting an existing uploaded file.
+ *
+ * @param string $directory The directory to which the file is moved
+ * @param UploadedFileInterface $uploadedFile The file uploaded file to move
+ *
+ * @return string The filename of moved file
+ */
+function moveUploadedFile(string $directory, UploadedFileInterface $uploadedFile)
+{
+    $extension = pathinfo($uploadedFile->getClientFilename(), PATHINFO_EXTENSION);
+    $basename = bin2hex(random_bytes(8));
+    $filename = sprintf('%s.%0.8s', $basename, $extension);
 
+    $uploadedFile->moveTo($directory . DIRECTORY_SEPARATOR . $filename);
+
+    return $filename;
+}
 
 $app->run();
